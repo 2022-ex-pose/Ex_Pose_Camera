f6a2f46a158fac3f690a40ed3e6dc52410ff8549
minhae <1813817@sookmyung.ac.kr>
2023-02-12 01:39:28 +0900

    2/12

--- screens/Walkthrough/Login.js
+++ screens/Walkthrough/Login.js
@@ -34,27 +34,48 @@ class Login extends React.Component {
   
 
     const CLIENT_ID = "560569442d62d727bea35653d92e561f";
-    const REDIRECT_URI =  "http://52.79.250.39:8080/oauth/callback/kakao";
+    const BaseUrl = "http://52.79.250.39:8080";
+    const REDIRECT_URI =  `${BaseUrl}/oauth/callback/kakao`;
 
-   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const userInfoApi = `${BaseUrl}/user/me`;
 
     if(url.includes('code=')){
       // const [data, setData] = React.useState([]);
 
       console.log(url);
       console.log(code);
+
+      //전체 데이터 확인 및 access token 저장용 2/11 토큰 불러내기 성공
       fetch(`${KAKAO_AUTH_URL}`)
       .then((res) => res.json())
-      // .then((resJson)=>{setData(resJson.data)})
       .then(res => {
       console.log(res)
+      const token = res.data.jwtToken
+      console.log(token)
+
+      //사용자 데이터 fetch 정보 호출 완료
+      fetch(`${userInfoApi}`, {
+          method : "GET",
+          headers : {
+            Authorization : `Bearer ${token}`
+          }
+        }) 
+        .then((res) => res.json())
+        .then(res => {
+        const userData = res
+        console.log(userData)
+        })
+        .catch(console.error)
       })
       .catch(console.error)
+
+    
+
       this.webview.stopLoading();
       this.props.navigation.navigate('Walkthrough');
 
     }
-
   }
 
 }
