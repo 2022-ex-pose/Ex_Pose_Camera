7d82ad7ea180c62c4ed3c2c6f102d68ea18f5f56
yoonjeongin <yoonjeongin@naver.com>
2023-03-01 17:33:20 +0900

    [DESIGN] Welcome & Walkthrough CSS 적용

--- screens/Walkthrough/Walkthrough.js
+++ screens/Walkthrough/Walkthrough.js
@@ -1,6 +1,7 @@
 import { correctBorderRadius } from 'framer-motion/types/render/dom/projection/scale-correction';
 import React from 'react';
 import {
+    StyleSheet,
     View,
     Text,
     Animated
@@ -11,36 +12,104 @@ import { COLORS, SIZES, constants, FONTS } from "../../constants";
 import Walkthrough1 from './Walkthrough1';
 import {useRoute} from "@react-navigation/native";
 
+const styles = StyleSheet.create({
+    bg: {
+        flex: 1,
+        backgroundColor: COLORS.bgMain,
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    userTxt: {
+        fontFamily: 'HeirofLightOTFBold',
+        color: COLORS.txtMain,
+        fontSize: SIZES.h2,
+        textAlign: 'center',
+        marginTop: 80
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: SIZES.height * 0.2,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: SIZES.padding,
+        paddingVertical: SIZES.height > 700 ? SIZES.padding : 20
+    },
+    footerBtn1Container: {
+        flex: 1,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.lightGrey,
+        marginHorizontal: 10
+    },
+    footerBtn2Container: {
+        flex: 1,
+        marginleft: SIZES.radius,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.txtMain,
+        marginHorizontal: 10
+    },
+    footerBtn1Txt: {
+        fontFamily: 'HeirofLightOTFRegular',
+        fontSize: SIZES.h3,
+        color: COLORS.txtGrey,
+        // ...FONTS.h3
+    },
+    footerBtn2Txt: {
+        fontFamily: 'HeirofLightOTFRegular',
+        fontSize: SIZES.h3,
+        color: COLORS.bgMain,
+        // ...FONTS.h3
+    },
+    txt1: {
+        fontFamily: 'HeirofLightOTFBold',
+        fontSize: SIZES.h1,
+        // ...FONTS.h1,
+        color: COLORS.txtMain
+    },
+    txt2: {
+        fontFamily: 'HeirofLightOTFBold',
+        fontSize: SIZES.body3,
+        marginTop: SIZES.radius,
+        textAlign: 'center',
+        // ...FONTS.body3,
+        color: COLORS.txtGrey
+    }
+    
+});
+
 
 const Walkthrough = ({route, navigation}) => {
     //API정보 이용 시
-    const [userName, setUserName] = React.useState("");
+    const [userName, setUserName] = React.useState("Ex.Pose");
 
-    //토큰 전달 완료
-    const {token} = route.params;
+    // //토큰 전달 완료
+    // const {token} = route.params;
 
-    //User정보 호출 완료
-    const userNameInfo = () => {
+    // //User정보 호출 완료
+    // const userNameInfo = () => {
 
-        const BaseUrl = "http://52.79.250.39:8080";
-        const userInfoApi = `${BaseUrl}/user/me`;
+    //     const BaseUrl = "http://52.79.250.39:8080";
+    //     const userInfoApi = `${BaseUrl}/user/me`;
 
-        //사용자 데이터 fetch 정보 호출 완료
-        fetch(`${userInfoApi}`, {
-                method : "GET",
-                headers : {
-                    Authorization : `Bearer ${token}`
-                }
-                }) 
-                .then((res) => res.json())
-                .then(res => {
-                setUserName(res.nickname)
-                console.log(userName)
-                })
-                .catch(console.error)
-    }
+    //     //사용자 데이터 fetch 정보 호출 완료
+    //     fetch(`${userInfoApi}`, {
+    //             method : "GET",
+    //             headers : {
+    //                 Authorization : `Bearer ${token}`
+    //             }
+    //             }) 
+    //             .then((res) => res.json())
+    //             .then(res => {
+    //             setUserName(res.nickname)
+    //             console.log(userName)
+    //             })
+    //             .catch(console.error)
+    // }
 
-    userNameInfo();
+    // userNameInfo();
 
     const scrollX = React.useRef(new Animated.Value(0)).current;
 
@@ -59,7 +128,7 @@ const Walkthrough = ({route, navigation}) => {
                     constants.walkthrough.map((item, index) => {
                         const dotColor = dotPostition.interpolate({
                             inputRange: [index -1, index, index+1],
-                            outputRange: [COLORS.dark08, COLORS.primary, COLORS.dark08],
+                            outputRange: [COLORS.txtGrey, COLORS.txtMain, COLORS.txtGrey],
                             extrapolate: "clamp"
                         })
 
@@ -84,17 +153,7 @@ const Walkthrough = ({route, navigation}) => {
     function renderFooter() {
         return (
             <View
-                style={{
-                    position: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    height: SIZES.height * 0.2,
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    paddingHorizontal: SIZES.padding,
-                    paddingVertical: SIZES.height > 700 ? SIZES.padding : 20
-                }}
+                style={styles.footer}
                 >
 
                 <Dots />
@@ -108,31 +167,18 @@ const Walkthrough = ({route, navigation}) => {
                 >
                     <TextButton
                         label="추천없이 사용"
-                        contentContainerStyle={{
-                            flex: 1,
-                            borderRadius: SIZES.radius,
-                            backgroundColor: COLORS.lightGrey
-                        }}
-                        labelStyle={{
-                            color: COLORS.primary,
-                            ...FONTS.h3
-                        }}
+                        contentContainerStyle={styles.footerBtn1Container}
+                        labelStyle={styles.footerBtn1Txt}
                         onPress={() => {
-                            navigation.navigate("Home", {token: token})
+                            navigation.navigate("Home")
+                            // navigation.navigate("Home", {token: token})
                     }}
                     />
                     {/*원래 로그인 이었던 것*/}
                     <TextButton
                         label="포즈 추천받기" 
-                        contentContainerStyle={{
-                            flex: 1,
-                            marginleft: SIZES.radius,
-                            borderRadius: SIZES.radius,
-                            backgroundColor: COLORS.primary
-                        }}
-                        labelStyle={{
-                            ...FONTS.h3
-                        }}
+                        contentContainerStyle={styles.footerBtn2Container}
+                        labelStyle={styles.footerBtn2Txt}
                     />
 
                 </View>
@@ -142,12 +188,9 @@ const Walkthrough = ({route, navigation}) => {
     return (
        
         <View
-            style={{
-                flex: 1,
-                backgroundColor: COLORS.light
-            }}
+            style={styles.bg}
         >
-            <Text>
+            <Text style={styles.userTxt}>
                 {userName}님 반갑습니다!
             </Text>
             <Animated.FlatList
@@ -194,20 +237,13 @@ const Walkthrough = ({route, navigation}) => {
                                 }}
                             >
                                 <Text
-                                    style={{
-                                        ...FONTS.h1
-                                    }}
+                                    style={styles.txt1}
                                     >
                                     {item.title}
                                 </Text>
 
                                 <Text
-                                    style={{
-                                        marginTop: SIZES.radius,
-                                        textAlign: 'center',
-                                        ...FONTS.body3,
-                                        color: COLORS.grey
-                                    }}
+                                    style={styles.txt2}
                                 >
                                     {item.sub_title}        
                                 </Text>
